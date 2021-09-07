d9b367b8c1649fb8949a55009794a4e28542042a
egorkaras1997@gmail.com <egorkaras1997@gmail.com>
2021-09-07 17:52:13 +0300

    Add new Layaout and some nuget packets

--- MassageSalon.WEB/Startup.cs
+++ MassageSalon.WEB/Startup.cs
@@ -54,7 +54,6 @@ namespace MassageSalon.WEB
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -64,7 +63,6 @@ namespace MassageSalon.WEB
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
