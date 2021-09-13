0e829bb32f36594c401e240e1012415199b971a8
egorkaras1997@gmail.com <egorkaras1997@gmail.com>
2021-09-13 17:55:31 +0300

    Add validator

--- MassageSalon.WEB/Validators/RecordValidator.cs
+++ MassageSalon.WEB/Validators/RecordValidator.cs
@@ -14,19 +14,10 @@ namespace MassageSalon.WEB.Validators
         private  RecordService recordService;
         public RecordValidator()
         {
-            RuleFor(c => c.TimeRecord).NotEmpty().Must(timeRecord => ).WithMessage("Please record to future date");
+            RuleFor(c => c.TimeRecord).NotEmpty().Must(NotLessThanNow).WithMessage("Please record to future date");
         }
         private bool NotLessThanNow(DateTime date) => date > DateTime.Now;
 
-        RuleFor(x => x.Id)
-    .Must(id =>
-    {
-            using (var db = HostContext.AppHost.GetDbConnection(base.Request))
-            {
-                return !db.Exists<EntityName>(x => x.Id == id);
-            }
-        })
-    .WithErrorCode("AlreadyExists")
-    .WithMessage("...");
+    
     }
 }
