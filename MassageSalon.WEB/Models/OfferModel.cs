ade3cef5e5189cd3d569250763d8b51035200e6d
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-10-06 02:16:58 +0300

    Add OfferModel and take it for mappping

---
+++ MassageSalon.WEB/Models/OfferModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MassageSalon.WEB.Models
+{
+    public class OfferModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "The Length of the title can't be more than 30 symbols")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
