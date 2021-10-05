c4940d615772c4611a93f818b7360a62de9fd52f
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-10-06 02:40:46 +0300

    Add Index View

--- MassageSalon.WEB/Controllers/OfferController.cs
+++ MassageSalon.WEB/Controllers/OfferController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using MassageSalon.BLL.Interfaces;
+using MassageSalon.DAL.Common.Entities;
+using MassageSalon.WEB.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MassageSalon.WEB.Controllers
 {
-    public class OfferController : Controller
+    [Authorize]
+    public class OfferController : BaseController
     {
+        private readonly IOfferService _offerService;
+        private readonly IMapper _mapper;
+        public OfferController(IOfferService offerService, IMapper mapper)
+        {
+            _offerService = offerService;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
-            return View();
+            var offers = _offerService.GetAll();
+            return View(_mapper.Map<IEnumerable<Offer>,IEnumerable<OfferModel>>(offers));
         }
     }
 }
