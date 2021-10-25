3c038e618e4e7504e1716effef0c09810fb072c2
egorkaras1997@gmail.com <egorkaras1997@gmail.com>
2021-10-25 17:35:49 +0300

    VisitorService To Async

--- MassageSalon.BLL/Interfaces/IVisitorService.cs
+++ MassageSalon.BLL/Interfaces/IVisitorService.cs
@@ -2,16 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MassageSalon.BLL.Interfaces
 {
     public interface IVisitorService
     {
-        void Create(Visitor visitor);
-        Visitor GetById(int id);
-        IEnumerable<Visitor> GetAll();
-        void Update(Visitor visitor);
-        void Delete(int id);
+        Task CreateAsync(Visitor visitor);
+        Task<Visitor> GetByIdAsync(int id);
+        Task<IEnumerable<Visitor>> GetAllAsync();
+        Task UpdateAsync(Visitor visitor);
+        Task DeleteAsync(int id);
         Visitor Get(Func<Visitor, bool> predicate);
         Visitor GetWithInclude();
         Visitor GetWithInclude(int id);
