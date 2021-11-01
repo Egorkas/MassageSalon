0f9d0f1512aa37147682fee7715cd80262ac0063
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-11-02 02:49:10 +0300

    Add xUnitProject and reference

---
+++ MassageSalon.Test/AdvancedSearchTest.cs
@@ -0,0 +1,16 @@
+using MassageSalon.DAL.Common.Entities;
+using System;
+using Xunit;
+
+namespace MassageSalon.Test
+{
+    public class AdvancedSearchTest
+    {
+
+        [Fact]
+        public void Test()
+        {
+            var offer = new Offer();
+        }
+    }
+}
